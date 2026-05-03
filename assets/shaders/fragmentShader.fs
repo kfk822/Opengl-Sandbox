f86fed6fdef2ec0875623784a5294414dbb23829
Krisztián <kfk8222@gmail.com>
2026-05-03 18:06:57 +0200

    Textures exercise1

--- assets/shaders/fragmentShader.fs
+++ assets/shaders/fragmentShader.fs
@@ -5,10 +5,13 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
+vec2 mTexCoord;
+
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord),texture(texture2, TexCoord), 0.2);
+    mTexCoord= vec2(-TexCoord.x,TexCoord.y);
+    FragColor = mix(texture(texture1, TexCoord),texture(texture2, mTexCoord), 0.2);
 }
